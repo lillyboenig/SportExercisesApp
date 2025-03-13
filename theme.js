87558a8f6c1950108edf15bb5ba87315e842b0d5
lillyboenig <lilly.boenig@web.de>
2025-03-13 15:28:12 +0200

    Add custom React Native Paper theme

---
+++ theme.js
@@ -0,0 +1,14 @@
+
+import { MD3LightTheme as DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#8A2BE2',   // a purple shade
+    secondary: '#FF69B4', // a pink shade
+  
+  },
+  roundness: 10,
+ 
+};
